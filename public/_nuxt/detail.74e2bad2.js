df5e5eb6e703653ab0cdb926ff11e8f830e38e38
CetusDevelopment <sunnykim888@proton.me>
2023-05-04 15:58:16 +0800

    update

--- public/_nuxt/detail.74e2bad2.js
+++
@@ -1,1 +0,0 @@
-import{eP as e}from"./entry.89e1e41e.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
