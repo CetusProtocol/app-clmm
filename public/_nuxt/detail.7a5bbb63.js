0290b686de857d3d1566378e0c742eed981b8e49
CetusDevelopment <sunnykim888@proton.me>
2023-05-17 15:37:47 +0800

    update

--- public/_nuxt/detail.7a5bbb63.js
+++
@@ -1,1 +0,0 @@
-import{e_ as e}from"./entry.1ec3126e.js";const a=e((t,r)=>{const o=document.querySelector(".body-container-bg");o&&o.scrollTo({top:0})});export{a as default};
--- public/_nuxt/icon-Rewards.466f10fc.js
+++
@@ -1,1 +0,0 @@
-import"./entry.1ec3126e.js";const e=""+new URL("icon-Rewards.add64e7c.png",import.meta.url).href;export{e as _};
