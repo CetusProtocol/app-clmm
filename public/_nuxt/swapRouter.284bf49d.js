e84513e7fa4cf5d3adb0bbd0001a3da1086bdfe4
CetusDevelopment <sunnykim888@proton.me>
2023-06-02 17:53:46 +0800

    update

--- public/_nuxt/swapRouter.284bf49d.js
+++
@@ -1,1 +0,0 @@
-import{a1 as e}from"./entry.87554927.js";import{a}from"./pool.caea2355.js";const u=e("swapRouter",{state:()=>({swapRouterContract:{},isInitRouter:!0}),actions:{initSwapRouter(t,r,o){this.swapRouterContract=a(t),this.swapRouterContract.initSwapRouter(r,o),this.isInitRouter=!1}}});export{u};
---
+++ public/_nuxt/swapRouter.d781d6ec.js
@@ -0,0 +1,1 @@
+import{a1 as e}from"./entry.16e77510.js";import{a}from"./pool.723017bd.js";const u=e("swapRouter",{state:()=>({swapRouterContract:{},isInitRouter:!0}),actions:{initSwapRouter(t,r,o){this.swapRouterContract=a(t),this.swapRouterContract.initSwapRouter(r,o),this.isInitRouter=!1}}});export{u};
