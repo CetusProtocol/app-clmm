fff730b0eb24c7bf07a6d7b75ccea5e6a855de4c
CetusDevelopment <sunnykim888@proton.me>
2023-07-21 11:17:48 +0800

    update

---
+++ public/_nuxt/decimal.38376165.js
@@ -0,0 +1,1 @@
+import{D as o}from"./pool.64e264f2.js";o.config({precision:64,rounding:o.ROUND_DOWN,toExpNeg:-64,toExpPos:64});
---
+++ public/_nuxt/swapRouter.a8ad7325.js
@@ -0,0 +1,1 @@
+import{a1 as e}from"./entry.8fda5278.js";import{a}from"./pool.64e264f2.js";const u=e("swapRouter",{state:()=>({swapRouterContract:{},isInitRouter:!0}),actions:{initSwapRouter(t,r,o){this.swapRouterContract=a(t),this.swapRouterContract.initSwapRouter(r,o),this.isInitRouter=!1}}});export{u};
